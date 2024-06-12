4859616d8808a7603ecb19f40503c3d2932f2df8
Yuuki <153100258+yuuki-dn@users.noreply.github.com>
2024-06-12 18:38:44 +0700

    abc

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-java -Xmx1536M -Xms1536M -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar spigot-1.20.6.jar nogui
+java -Xmx1536M -Xms1536M \
+    -XX:+AlwaysPreTouch -XX:+DisableExplicitGC \
+    -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem \ 
+    -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC \ 
+    -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 \ 
+    -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 \ 
+    -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 \ 
+    -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 \
+    -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 \ 
+    -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true \
+    -jar paper-1.20.4-497.jar nogui
